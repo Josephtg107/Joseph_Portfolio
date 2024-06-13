c55dcd00f7b9700c6153b6ff7e310b110ed0d032
José Teodoro García Facundo <josephtg107@gmail.com>
2024-06-12 20:38:25 -0500

    [JS]Updated swiper JS for functionality

--- script.js
+++ script.js
@@ -54,3 +54,17 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+//Swiper JS
+var swiper = new Swiper(".mySwiper", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
